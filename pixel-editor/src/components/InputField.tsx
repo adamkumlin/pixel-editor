fc1044887dcd3aad29c7024a7b688130eb08688b
Adam Kumlin <skola.adamkumlin99@gmail.com>
2023-11-06 11:33:00 +0100

    style inputs

--- pixel-editor/src/components/InputField.tsx
+++ pixel-editor/src/components/InputField.tsx
@@ -2,19 +2,23 @@ import { ChangeEventHandler } from "react";
 import "../App.css";
 
 type InputFieldProps = {
-    label: string;
-    type: string;
-    value: any;
-    onChange: ChangeEventHandler<HTMLInputElement>;
-    
-}
-const InputField: React.FC<InputFieldProps> = ({label, type, value, onChange}) => {
-
+  label: string;
+  type: string;
+  value: any;
+  onChange: ChangeEventHandler<HTMLInputElement>;
+};
+const InputField: React.FC<InputFieldProps> = ({
+  label,
+  type,
+  value,
+  onChange,
+}) => {
   return (
     <div className="InputField">
       <label>
         {label}
         <input
+          className="Field"
           type={type}
           value={value}
           onChange={onChange}
